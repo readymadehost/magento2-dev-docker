029553d352c9597c66cdd717c150ed9534144b47
Suson Waiba <susonwaiba@gmail.com>
2021-03-21 01:31:52 +0545

    docker setup for magento2.4.x

---
+++ php7.4/cli/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/root/manage-project-permission.sh
+
+if [ ${PHP_ENABLE_REDIS} -eq "1" ]; then
+    docker-php-ext-enable redis
+    echo "-- php cli redis extension enabled"
+fi
+
+if [ ${PHP_ENABLE_XDEBUG} -eq "1" ]; then
+    docker-php-ext-enable xdebug
+    echo "-- php cli xdebug extension enabled"
+fi
+
+tail -f /dev/null
