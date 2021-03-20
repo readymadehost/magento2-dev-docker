018ac0a013757aab5cdb28afafd48c98bb9cfbd0
Suson Waiba <susonwaiba@gmail.com>
2021-03-21 04:05:23 +0545

    updates for php7.4

--- php7.4/cli/docker-entrypoint.sh
+++ php7.4/cli/docker-entrypoint.sh
@@ -3,13 +3,13 @@
 /root/manage-project-permission.sh
 
 if [ ${PHP_ENABLE_REDIS} -eq "1" ]; then
-    docker-php-ext-enable redis
-    echo "-- php cli redis extension enabled"
+  docker-php-ext-enable redis
+  echo "-- php cli redis extension enabled"
 fi
 
 if [ ${PHP_ENABLE_XDEBUG} -eq "1" ]; then
-    docker-php-ext-enable xdebug
-    echo "-- php cli xdebug extension enabled"
+  docker-php-ext-enable xdebug
+  echo "-- php cli xdebug extension enabled"
 fi
 
 tail -f /dev/null
--- php7.4/fpm/docker-entrypoint.sh
+++ php7.4/fpm/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ${PHP_ENABLE_REDIS} -eq "1" ]; then
-    docker-php-ext-enable redis
-    echo "-- php fpm redis extension enabled"
+  docker-php-ext-enable redis
+  echo "-- php fpm redis extension enabled"
 fi
 
 if [ ${PHP_ENABLE_XDEBUG} -eq "1" ]; then
-    docker-php-ext-enable xdebug
-    echo "-- php fpm xdebug extension enabled"
+  docker-php-ext-enable xdebug
+  echo "-- php fpm xdebug extension enabled"
 fi
 
 exec "$@"
